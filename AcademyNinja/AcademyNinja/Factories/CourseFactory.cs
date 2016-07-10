3677f03f7412b390a1b6caadc98627f25eb6e603
sguzunov <sguzunov@gmail.com>
2016-07-10 03:13:23 +0300

    Add course factory.

---
+++ AcademyNinja/AcademyNinja/Factories/CourseFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace AcademyNinja.Factories
+{
+    using System;
+
+    using AcademyNinja.GameObjects.Contracts;
+    using Common;
+    using GameObjects.Courses;
+
+    internal class CourseFactory : ICourseFactory
+    {
+        private const string InvalidCourseErrorMesssage = "Course {0} does not exists!";
+
+        public ICourse CreateCourse(CourseType type)
+        {
+            switch (type)
+            {
+                case CourseType.HTML:
+                    return new HtmlCourse();
+                case CourseType.CSharp:
+                    return new CSharpCourse();
+                case CourseType.CSS:
+                    return new CSSCourse();
+                case CourseType.JavaScript:
+                    return new JavaScriptCourse();
+                default:
+                    throw new ArgumentException(string.Format(InvalidCourseErrorMesssage, type));
+            }
+        }
+    }
+}
---
+++ AcademyNinja/AcademyNinja/Factories/ICourseFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace AcademyNinja.Factories
+{
+    using AcademyNinja.GameObjects.Contracts;
+    using Common;
+
+    internal interface ICourseFactory
+    {
+        ICourse CreateCourse(CourseType type);
+    }
+}
